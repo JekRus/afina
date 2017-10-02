adf734fa3a3fb7e8b206dc0f1adf994febd0835f
Artur Elokhov <elohov-artur@yandex.ru>
2017-10-03 00:34:37 +0300

    hw3 alpha

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,18 @@
 #include <iostream>
 #include <memory>
 #include <uv.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <cstring>
 
 #include <afina/Storage.h>
 #include <afina/Version.h>
 
 #include "network/Server.h"
 #include "storage/MapBasedGlobalLockImpl.h"
+
+#define BUFFSIZE 4096
 
 typedef struct {
     std::shared_ptr<Afina::Storage> storage;
@@ -29,7 +35,64 @@ void timer_handler(uv_timer_t *handle) {
 }
 
 int main(int argc, char **argv) {
-    std::cout << "Starting Afina " << Afina::Version_Major << "." << Afina::Version_Minor << "."
+    if(argc > 4) {
+		std::cout << "Usage ./afina [-d] [-p <filepath>]\n";
+		return 1;
+	}
+	bool is_p = false;
+	bool is_d = false;
+	int i = 1;
+	std::string filepath;
+	while(i < argc) {
+		if((std::string(argv[i]) == "-d") && !is_d) {
+			is_d = true;
+		}
+		else if((std::string(argv[i]) == "-p") && !is_p && (++i < argc)) {
+			is_p = true;
+			filepath = std::string(argv[i]);
+		}
+		else {
+			std::cout << "Usage ./afina [-d] [-p <filepath>]\n";
+			return 1;
+		}
+		++i;
+	}
+	if(is_d) {
+		//parent
+		if(fork()) {
+			return 0;
+		}
+		//child
+		if(setsid() == -1) {
+			std::cerr << "Error. Cannot create new session\n";
+		}
+		if(close(STDIN_FILENO) || close(STDOUT_FILENO)) {
+			std::cerr << "Error. Cannot close STDIN/STDOUT\n";
+			return 1;
+			
+		}
+	}
+	if(is_p) {
+		char buffer[BUFFSIZE] = {0};
+		int fd;
+		fd = open(filepath.c_str(), O_WRONLY | O_CREAT, 0644);
+		if(fd == -1) {
+			std::cerr << "Error. Cannot open file\n";
+			return 1;
+		}
+		pid_t pid = getpid();
+		sprintf(buffer, "%d", pid);
+		if(write(fd, buffer, strlen(buffer)) == -1) {
+			std::cerr << "Error. Write to file failed\n";
+			return 1;
+		}
+		if(close(fd)) {
+			std::cerr << "Error. Cannot close file\n";
+			return 1;
+		}
+	}	
+	
+	std::cout << "Starting Afina " << Afina::Version_Major << "." << Afina::Version_Minor << "."
               << Afina::Version_Patch;
     if (Afina::Version_SHA.size() > 0) {
         std::cout << "-" << Afina::Version_SHA;
