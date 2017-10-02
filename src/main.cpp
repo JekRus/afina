cd6ba134f9918c9082c5e461fa8b1abf64fa1b91
JekRus <elohov-artur@yandex.ru>
2017-10-02 21:45:21 +0000

    hw3 formatted

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <chrono>
+#include <cstring>
+#include <fcntl.h>
 #include <iostream>
 #include <memory>
-#include <uv.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <cstring>
+#include <uv.h>
 
 #include <afina/Storage.h>
 #include <afina/Version.h>
@@ -35,64 +35,61 @@ void timer_handler(uv_timer_t *handle) {
 }
 
 int main(int argc, char **argv) {
-    if(argc > 4) {
-		std::cout << "Usage ./afina [-d] [-p <filepath>]\n";
-		return 1;
-	}
-	bool is_p = false;
-	bool is_d = false;
-	int i = 1;
-	std::string filepath;
-	while(i < argc) {
-		if((std::string(argv[i]) == "-d") && !is_d) {
-			is_d = true;
-		}
-		else if((std::string(argv[i]) == "-p") && !is_p && (++i < argc)) {
-			is_p = true;
-			filepath = std::string(argv[i]);
-		}
-		else {
-			std::cout << "Usage ./afina [-d] [-p <filepath>]\n";
-			return 1;
-		}
-		++i;
-	}
-	if(is_d) {
-		//parent
-		if(fork()) {
-			return 0;
-		}
-		//child
-		if(setsid() == -1) {
-			std::cerr << "Error. Cannot create new session\n";
-		}
-		if(close(STDIN_FILENO) || close(STDOUT_FILENO)) {
-			std::cerr << "Error. Cannot close STDIN/STDOUT\n";
-			return 1;
-			
-		}
-	}
-	if(is_p) {
-		char buffer[BUFFSIZE] = {0};
-		int fd;
-		fd = open(filepath.c_str(), O_WRONLY | O_CREAT, 0644);
-		if(fd == -1) {
-			std::cerr << "Error. Cannot open file\n";
-			return 1;
-		}
-		pid_t pid = getpid();
-		sprintf(buffer, "%d", pid);
-		if(write(fd, buffer, strlen(buffer)) == -1) {
-			std::cerr << "Error. Write to file failed\n";
-			return 1;
-		}
-		if(close(fd)) {
-			std::cerr << "Error. Cannot close file\n";
-			return 1;
-		}
-	}	
-	
-	std::cout << "Starting Afina " << Afina::Version_Major << "." << Afina::Version_Minor << "."
+    if (argc > 4) {
+        std::cout << "Usage ./afina [-d] [-p <filepath>]\n";
+        return 1;
+    }
+    bool is_p = false;
+    bool is_d = false;
+    int i = 1;
+    std::string filepath;
+    while (i < argc) {
+        if ((std::string(argv[i]) == "-d") && !is_d) {
+            is_d = true;
+        } else if ((std::string(argv[i]) == "-p") && !is_p && (++i < argc)) {
+            is_p = true;
+            filepath = std::string(argv[i]);
+        } else {
+            std::cout << "Usage ./afina [-d] [-p <filepath>]\n";
+            return 1;
+        }
+        ++i;
+    }
+    if (is_d) {
+        // parent
+        if (fork()) {
+            return 0;
+        }
+        // child
+        if (setsid() == -1) {
+            std::cerr << "Error. Cannot create new session\n";
+        }
+        if (close(STDIN_FILENO) || close(STDOUT_FILENO)) {
+            std::cerr << "Error. Cannot close STDIN/STDOUT\n";
+            return 1;
+        }
+    }
+    if (is_p) {
+        char buffer[BUFFSIZE] = {0};
+        int fd;
+        fd = open(filepath.c_str(), O_WRONLY | O_CREAT, 0644);
+        if (fd == -1) {
+            std::cerr << "Error. Cannot open file\n";
+            return 1;
+        }
+        pid_t pid = getpid();
+        sprintf(buffer, "%d", pid);
+        if (write(fd, buffer, strlen(buffer)) == -1) {
+            std::cerr << "Error. Write to file failed\n";
+            return 1;
+        }
+        if (close(fd)) {
+            std::cerr << "Error. Cannot close file\n";
+            return 1;
+        }
+    }
+
+    std::cout << "Starting Afina " << Afina::Version_Major << "." << Afina::Version_Minor << "."
               << Afina::Version_Patch;
     if (Afina::Version_SHA.size() > 0) {
         std::cout << "-" << Afina::Version_SHA;
