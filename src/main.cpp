4775b12729276a7946b1d575edcf99ebe267abc0
Artur Elokhov <elohov-artur@yandex.ru>
2017-10-05 16:42:23 +0300

    hw3 rewrited

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <fcntl.h>
 #include <iostream>
+#include <fstream>
 #include <memory>
 #include <stdio.h>
 #include <unistd.h>
@@ -39,73 +40,11 @@ void timer_handler(uv_timer_t *handle) {
 }
 
 int main(int argc, char **argv) {
-/*
-<<<<<<< HEAD
-    if (argc > 4) {
-        std::cout << "Usage ./afina [-d] [-p <filepath>]\n";
-        return 1;
-    }
-    bool is_p = false;
-    bool is_d = false;
-    int i = 1;
-    std::string filepath;
-    while (i < argc) {
-        if ((std::string(argv[i]) == "-d") && !is_d) {
-            is_d = true;
-        } else if ((std::string(argv[i]) == "-p") && !is_p && (++i < argc)) {
-            is_p = true;
-            filepath = std::string(argv[i]);
-        } else {
-            std::cout << "Usage ./afina [-d] [-p <filepath>]\n";
-            return 1;
-        }
-        ++i;
-    }
-    if (is_d) {
-        // parent
-        if (fork()) {
-            return 0;
-        }
-        // child
-        if (setsid() == -1) {
-            std::cerr << "Error. Cannot create new session\n";
-        }
-        if (close(STDIN_FILENO) || close(STDOUT_FILENO)) {
-            std::cerr << "Error. Cannot close STDIN/STDOUT\n";
-            return 1;
-        }
-    }
-    if (is_p) {
-        char buffer[BUFFSIZE] = {0};
-        int fd;
-        fd = open(filepath.c_str(), O_WRONLY | O_CREAT, 0644);
-        if (fd == -1) {
-            std::cerr << "Error. Cannot open file\n";
-            return 1;
-        }
-        pid_t pid = getpid();
-        sprintf(buffer, "%d", pid);
-        if (write(fd, buffer, strlen(buffer)) == -1) {
-            std::cerr << "Error. Write to file failed\n";
-            return 1;
-        }
-        if (close(fd)) {
-            std::cerr << "Error. Cannot close file\n";
-            return 1;
-        }
-    }
-
-    std::cout << "Starting Afina " << Afina::Version_Major << "." << Afina::Version_Minor << "."
-              << Afina::Version_Patch;
-=======
-*/
-    // Build version
+	// Build version
     // TODO: move into Version.h as a function
     std::stringstream app_string;
     app_string << "Afina " << Afina::Version_Major << "." << Afina::Version_Minor << "." << Afina::Version_Patch;
-//>>>>>>> d1889550123c497a6de29856d5b3958b58228cd6
-
-    if (Afina::Version_SHA.size() > 0) {
+	if (Afina::Version_SHA.size() > 0) {
         app_string << "-" << Afina::Version_SHA;
     }
 
@@ -116,6 +55,8 @@ int main(int argc, char **argv) {
         // and simplify validation below
         options.add_options()("s,storage", "Type of storage service to use", cxxopts::value<std::string>());
         options.add_options()("n,network", "Type of network service to use", cxxopts::value<std::string>());
+        options.add_options()("d,daemon", "Run as daemon");
+        options.add_options()("p,pid", "Write pid in file", cxxopts::value<std::string>());
         options.add_options()("h,help", "Print usage info");
         options.parse(argc, argv);
 
@@ -132,6 +73,33 @@ int main(int argc, char **argv) {
     Application app;
     std::cout << "Starting " << app_string.str() << std::endl;
 
+    //daemon and pid options
+    if(options.count("daemon") > 0) {
+		if (fork()) {
+            return 0;
+        }
+        // child
+        if (setsid() == -1) {
+            std::cerr << "Error. Cannot create new session\n";
+            return 1;
+        }
+        if (close(STDIN_FILENO) || close(STDOUT_FILENO) || close(STDERR_FILENO)) {
+            std::cerr << "Error. Cannot close STDIN/STDOUT\n";
+            return 1;
+        }
+    }
+    try {
+		if(options.count("pid") > 0) {
+			std::fstream fs;
+			fs.open(options["pid"].as<std::string>(), std::fstream::out | std::fstream::trunc);
+			fs << getpid();
+			fs.close();
+		}
+	} catch (std::exception &e) {
+        std::cerr << "Error" << e.what() << std::endl;
+    }
+    
+    
     // Build new storage instance
     std::string storage_type = "map_global";
     if (options.count("storage") > 0) {
