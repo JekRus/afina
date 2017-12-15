bf3ff69d4285f031046d812e77ae494894bd76ac
JekRus <elohov-artur@yandex.ru>
2017-12-15 13:27:15 +0000

    thread pool

--- src/executor/Executor.cpp
+++ src/executor/Executor.cpp
@@ -3,46 +3,39 @@
 #include <pthread.h>
 #include <stdexcept>
 
-
 namespace Afina {
 
 void perform(Executor *executor) {
-    while(true) {
+    while (true) {
         bool is_task = false;
         std::function<void()> task;
         std::unique_lock<std::mutex> queue_lock(executor->queue_mutex);
-        if(!executor->tasks.empty()) {
+        if (!executor->tasks.empty()) {
             task = executor->tasks.front();
             executor->tasks.pop_front();
             is_task = true;
-        }
-        else {
+        } else {
             std::unique_lock<std::mutex> state_lock(executor->state_mutex);
-            switch(executor->state) {
-                case Executor::State::kRun: {
-                    if(executor->tasks.empty()) {
-                        executor->empty_condition.wait(state_lock);
-                    }
+            switch (executor->state) {
+            case Executor::State::kRun: {
+                if (executor->tasks.empty()) {
+                    executor->empty_condition.wait(state_lock);
                 }
-                break;
-                case Executor::State::kStopping: {
-                    if(executor->tasks.empty()) {
-                        executor->state = Executor::State::kStopped;
-                        return;
-                    }
-                }
-                break;
-                case Executor::State::kStopped: {
+            } break;
+            case Executor::State::kStopping: {
+                if (executor->tasks.empty()) {
+                    executor->state = Executor::State::kStopped;
                     return;
                 }
-                break;
-                default: {
-                    throw std::runtime_error("Executor::State error");
-                }
+            } break;
+            case Executor::State::kStopped: {
+                return;
+            } break;
+            default: { throw std::runtime_error("Executor::State error"); }
             }
         }
         queue_lock.unlock();
-        if(is_task) {
+        if (is_task) {
             task();
         }
     }
@@ -51,7 +44,7 @@ void perform(Executor *executor) {
 Executor::Executor(std::string name, int size) {
     state = State::kRun;
     threads.reserve(size);
-    for(int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i) {
         threads.emplace_back(std::thread(perform, this));
     }
 }
@@ -61,29 +54,28 @@ Executor::~Executor() {
     state = State::kStopped;
     empty_condition.notify_all();
     state_lock.unlock();
-    for(auto &thread : threads) {
+    for (auto &thread : threads) {
         thread.join();
     }
 }
 
 void Executor::Stop(bool await) {
     std::unique_lock<std::mutex> state_lock(state_mutex);
-    if(tasks.empty()) {
+    if (tasks.empty()) {
         state = State::kStopped;
         empty_condition.notify_all();
-    }
-    else {
+    } else {
         state = State::kStopping;
         empty_condition.notify_all();
     }
     state_lock.unlock();
-    if(await) {
+    if (await) {
         auto iter = threads.begin();
-        while(iter != threads.end()) {
+        while (iter != threads.end()) {
             iter->join();
             threads.erase(iter);
         }
     }
 }
 
-}
+} // namespace Afina
